e698247fc8ed99782653740504e28a4585792710
Ryan Hamilton <rhamil4@gmu.edu>
2023-10-07 20:15:37 -0400

    moved code and added comments

--- popup/popup.js
+++ popup/popup.js
@@ -1,16 +1,24 @@
+// initialized as stable
+let deadLocked = false;
+
+// collect popup elements
+var slider = document.getElementById("slider");
+var logo = document.getElementById("logo");
+// restore state
+var scalar = await fetchScalar();
+var state = await fetchStatus();
+
+// adjust logo colors to state
+switchLogo();
+
+// enable tab binding for independent behavior
 async function getCurrentTab() {
   let queryOptions = { active: true, lastFocusedWindow: true };
   let [tab] = await chrome.tabs.query(queryOptions);
   return tab;
 }
 
-let deadLocked = false;
-
-function setStatusText(text) {
-  document.getElementById("status").innerHTML = text;
-  console.log(`setting status text to: ${text}`);
-}
-
+// serve information about state and return status (bool)
 async function fetchStatus() {
   const tab = await getCurrentTab();
   if (!tab) {
@@ -31,15 +39,7 @@ async function fetchStatus() {
   }
 }
 
-var state = await fetchStatus();
-var logo = document.getElementById("logo");
-
-function switchLogo() {
-  var file = deadLocked ? "_dead" : state ? "" : "_off"
-  logo.src = `../assets/filter8${file}.svg`;
-}
-
-var slider = document.getElementById("slider");
+// retrieve slider state
 async function fetchScalar() {
   const tab = await getCurrentTab();
   if (tab) {
@@ -57,21 +57,36 @@ async function fetchScalar() {
   }
 }
 
-var scalar = await fetchScalar();
+// write status to popup
+function setStatusText(text) {
+  document.getElementById("status").innerHTML = text;
+  console.log(`setting status text to: ${text}`);
+}
 
+// change logo depending on status
+function switchLogo() {
+  var file = deadLocked ? "_dead" : state ? "" : "_off"
+  logo.src = `../assets/filter8${file}.svg`;
+}
+
+// update state when slider is adjusted
 slider.addEventListener("change", async () => {
   const tab = await getCurrentTab();
   if (tab) {
     scalar = slider.value;
-    const response = await chrome.tabs.sendMessage(tab.id, {
-      action: "slider",
-      scale: scalar,
-    });
-    console.log(response);
+    try {
+      const response = await chrome.tabs.sendMessage(tab.id, {
+        action: "slider",
+        scale: scalar,
+      });
+      console.log(response);
+    } catch (error) {
+      console.log(error);
+    }
   }
 });
 
-switchLogo();
+// toggle extension when logo is pressed
 logo.addEventListener("click", async () => {
   const tab = await getCurrentTab();
   if (tab && !deadLocked) {
